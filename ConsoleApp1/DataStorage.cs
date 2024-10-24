4a5e64eb91b1786f83700d12c3e5b030bb7bce4f
Gfif <73363091+gfifgfifofich@users.noreply.github.com>
2024-10-24 16:51:12 +0300

    init

---
+++ ConsoleApp1/DataStorage.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+	Класс для обращения с данными как в классах, с возможностью сохранения и загрузки с файла.
+	Писал его для игрового движка, оттуда и названия (Save, Load, DataStorage, ect.)
+	Данная имплементация - грубый перевод с C++, однако Dictionary работает полностью как std::map, 
+	поэтому сам класс полностью безопасен по памяти, null значениям и прочему
+ */
+
+namespace ConsoleApp1
+{
+	internal class DataStorage
+	{
+		public  Dictionary<string, Dictionary<string, string>> data = new Dictionary<string, Dictionary<string, string>>();
+
+		public void AddObject(string ObjectName)
+        {
+            ObjectName = ObjectName.Trim();
+            data.TryAdd(ObjectName, new Dictionary<string, string>());
+		}
+		public void AddProperty(string ObjectName, string Property, string value = "")
+        {
+            ObjectName = ObjectName.Trim();
+            Property = Property.Trim();
+            data[ObjectName].TryAdd(Property, value);
+		}
+
+        public Dictionary<string, string> GetObject(string ObjectName)
+        {
+            ObjectName = ObjectName.Trim();
+            // если нету значения или объекта, создать пустой 
+            if (!data.ContainsKey(ObjectName))
+                AddObject(ObjectName);
+
+            return data[ObjectName];
+		}
+
+        public string GetProperty(string ObjectName, string Property)
+        {
+            ObjectName = ObjectName.Trim();
+            Property = Property.Trim();
+            // если нету значения или объекта, создать пустой 
+            if (!data.ContainsKey(ObjectName))
+                AddObject(ObjectName);
+            if (!data[ObjectName].ContainsKey(Property))
+                AddProperty(ObjectName, Property);
+
+            string prop = data[ObjectName][Property];
+            prop = prop.Trim();
+			return prop;
+		}
+        public int GetPropertyAsInt(string ObjectName, string Property)
+        {
+            ObjectName = ObjectName.Trim();
+            Property = Property.Trim();
+            return Convert.ToInt32(GetProperty(ObjectName, Property));
+		}
+        public float GetPropertyAsFloat(string ObjectName, string Property)
+        {
+            ObjectName = ObjectName.Trim();
+            Property = Property.Trim();
+            return Convert.ToSingle(GetProperty(ObjectName, Property));
+		}
+        public bool GetPropertyAsBool(string ObjectName, string Property)
+        {
+            ObjectName = ObjectName.Trim();
+            Property = Property.Trim();
+            return Convert.ToBoolean(GetProperty(ObjectName, Property));
+		}
+
+        public void SetProperty(string ObjectName, string Property, string value = "")
+        {
+            ObjectName = ObjectName.Trim();
+            Property = Property.Trim();
+            // ибо Dictionary не создаёт значения автоматически
+            if (!data.ContainsKey(ObjectName))
+                AddObject(ObjectName);
+            if (!data[ObjectName].ContainsKey(Property))
+                AddProperty(ObjectName, Property);
+            data[ObjectName][Property] = value;
+		}
+        public void SetProperty(string ObjectName, string Property, bool value)
+        {
+            ObjectName = ObjectName.Trim();
+            Property = Property.Trim();
+            // ибо Dictionary не создаёт значения автоматически
+            if (!data.ContainsKey(ObjectName))
+                AddObject(ObjectName);
+            if (!data[ObjectName].ContainsKey(Property))
+                AddProperty(ObjectName,Property);
+
+            data[ObjectName][Property] = Convert.ToString(value);
+		}
+        public void SetProperty(string ObjectName, string Property, int value)
+        {
+            ObjectName = ObjectName.Trim();
+            Property = Property.Trim();
+            // ибо Dictionary не создаёт значения автоматически
+            if (!data.ContainsKey(ObjectName))
+                AddObject(ObjectName);
+            if (!data[ObjectName].ContainsKey(Property))
+                AddProperty(ObjectName, Property);
+
+            data[ObjectName][Property] = Convert.ToString(value);
+		}
+        public void SetProperty(string ObjectName, string Property, float value)
+        {
+            ObjectName = ObjectName.Trim();
+            Property = Property.Trim();
+            // ибо Dictionary не создаёт значения автоматически
+            if (!data.ContainsKey(ObjectName))
+                AddObject(ObjectName);
+            if (!data[ObjectName].ContainsKey(Property))
+                AddProperty(ObjectName, Property);
+
+            data[ObjectName][Property] = Convert.ToString(value);
+		}
+
+        public void Save(string filename)
+		{
+			StreamWriter file = new StreamWriter(filename);
+			foreach (var e in data)
+			{
+				file.Write(e.Key + "\n" + "{" );
+
+				foreach (var p in e.Value)
+				{
+					file.Write("\n	" + p.Key + " " + p.Value + "");
+				}
+
+				file.Write("\n}\n");
+			}
+			file.Close();
+		}
+        public void Load(string filename)
+		{
+			data.Clear();
+
+			StreamReader sr = new StreamReader(filename);
+			
+
+			string lastObject = "NULL";
+
+			while (!sr.EndOfStream)
+			{
+				string line = "";
+				line = sr.ReadLine(); // VS пишет что возможен нулл, но нулл невозможен изза условия в цикле
+
+				if(line != null && line.Length == 0)
+					continue;
+
+				if (line[0] == '{')  // аналогично
+					continue;
+
+				// reset last object, "exit object"
+				if (line[0] == '}' && lastObject != "NULL")
+				{
+					lastObject = "NULL";
+                    continue;
+				}
+				// "enter object"
+				if (line[0] != '{' && line[0] != '}' && lastObject == "NULL")
+				{
+					lastObject = "";
+                    line = line.Trim();
+                    lastObject = line;
+					if (lastObject != "")
+						AddObject(lastObject);
+					continue;
+				}
+
+				if (lastObject != "NULL")
+				{
+					string property = "";
+					string value = "";
+
+                    line = line.Trim();
+                    int i = 0;
+                    while (i < line.Length && line[i] != ' ')
+                    {
+                        property += line[i];
+                        i++;
+                    }
+                    i++;// skip space;
+                    while (i < line.Length)
+                    {
+                        value += line[i];
+                        i++;
+                    }
+                    AddProperty(lastObject, property, value);
+					continue;
+				}
+			}
+            sr.Close();
+        }
+	}
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,186 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+namespace ConsoleApp1
+{
+	static class Program
+	{
+		// Функция в конечном варианте не потребовалась. Может пригодиться при создании данных. Использована только в логах, для одинакового формата с остальным тз
+		public static string TimeToFormatedString(DateTime dt)
+		{
+			string s = Convert.ToString(dt.Year) + "-" + Convert.ToString(dt.Month) + "-" + Convert.ToString(dt.Day);
+			string[] ss = dt.ToString().Split(' ');
+			s += " " + ss[1];
+			return s;
+		}
+
+		static void Main(string[] argv)
+		{            
+			DataStorage ConfigDS = new DataStorage();
+			ConfigDS.Load("./Config.txt");
+			bool showHelp = false;
+			int argvIter = 0;
+			foreach (string str in argv)
+			{
+				if (str == "-h")
+					showHelp = true;
+				if (str == "-l" && argvIter + 1 < argv.Length)
+					ConfigDS.SetProperty("Config", "_deliveryLog",argv[argvIter + 1]);
+				if (str == "-o" && argvIter + 1 < argv.Length)
+					ConfigDS.SetProperty("Config", "_deliveryOrder", argv[argvIter + 1]);
+				if (str == "-i" && argvIter + 1 < argv.Length)
+					ConfigDS.SetProperty("Config", "_deliveryInput", argv[argvIter + 1]);
+				if (str == "-d" && argvIter + 1 < argv.Length)
+					ConfigDS.SetProperty("Config", "_deliveryDistrict", argv[argvIter + 1]);
+				if (str == "-fdt" && argvIter + 2 < argv.Length)
+					ConfigDS.SetProperty("Config", "_deliveryTime", argv[argvIter + 1] + " " + argv[argvIter + 2]);
+				argvIter++;
+			}
+
+            DataStorage inputDS = new DataStorage();
+            DataStorage outputDS = new DataStorage();
+            DataStorage LogDS = new DataStorage(); // логи лучше было бы выводить просто в файл.
+
+            string _deloveryLog = ConfigDS.GetProperty("Config", "_deliveryLog");
+            try
+            {
+                // для сохранения предыдущих можно предварительно загрузить предыдущий файл
+                //LogDS.Load(_deloveryLog);
+                // для чистоты данной реализации, логи стираются после каждого запуска.
+                LogDS.SetProperty(TimeToFormatedString(DateTime.Now), DateTime.Now.TimeOfDay.ToString(),"	Deleted logs");
+                LogDS.Save(_deloveryLog);
+            }
+            catch
+            {
+                Console.WriteLine("Некорректный путь к файлу логов");
+                return;
+            }
+            string _deliveryOrder = ConfigDS.GetProperty("Config", "_deliveryOrder");
+            try
+            {
+                outputDS.Save(_deliveryOrder);
+                LogDS.SetProperty(TimeToFormatedString(DateTime.Now), DateTime.Now.TimeOfDay.ToString(), "	cleared output");
+                LogDS.Save(_deloveryLog);
+            }
+            catch
+            {
+                Console.WriteLine("Некорректный путь к файлу вывода фильтрации");
+                return;
+            }
+            string _deliveryInput = ConfigDS.GetProperty("Config", "_deliveryInput");
+            try
+            {
+                inputDS.Load(_deliveryInput);
+            }
+            catch
+            {
+                Console.WriteLine("Некорректный путь к файлу исходный данных");
+                return;
+            }
+
+
+            string _deliveryDistrict = ConfigDS.GetProperty("Config", "_deliveryDistrict");
+			if(_deliveryDistrict.Length ==0)
+			{
+                Console.WriteLine("Не указан район доставки");
+                return;
+            }
+
+			string _deliveryTime = ConfigDS.GetProperty("Config", "_deliveryTime");
+			DateTime deliveryTime = DateTime.Now;
+			try
+			{
+				deliveryTime = DateTime.Parse(_deliveryTime);
+			}
+			catch
+			{
+				Console.WriteLine("Некорректный формат времени/время доставки не указано");
+				Console.WriteLine("Формат yyyy-MM-dd HH:mm:ss");
+				return;
+			}
+
+			if (argv.Length == 1 || showHelp)
+			{
+				// help
+				Console.WriteLine("-h 					Вывести данное сообщение");
+				Console.WriteLine("-l path/to/log				Изменить путь для вывода логов");
+				Console.WriteLine("-o path/to/log				Изменить путь для вывода приложения");
+				Console.WriteLine("-i path/to/log				Изменить путь для файла с исходными данными");
+				Console.WriteLine("-d district_name			Изменить район доставки");
+				Console.WriteLine("-fdt yyyy-MM-dd HH:mm::ss		Изменить время первой доставки");
+				Console.WriteLine("Запуск без параметров будет использовать последние параметры (см. Config.txt)");
+
+				return;
+			}
+			ConfigDS.Save("./Config.txt");
+            LogDS.SetProperty(TimeToFormatedString(DateTime.Now), DateTime.Now.TimeOfDay.ToString(), "	Config saved");
+            LogDS.Save(_deloveryLog);
+
+            Console.WriteLine("Используемые параметры: ");
+            Console.WriteLine("_deloveryLog = " + _deloveryLog);
+            Console.WriteLine("_deliveryOrder = "+_deliveryOrder);
+            Console.WriteLine("_deliveryInput = "+_deliveryInput);
+            Console.WriteLine("_deliveryDistrict = "+_deliveryDistrict);
+            Console.WriteLine("_deliveryTime = " + _deliveryTime);
+            Console.WriteLine();
+
+
+            foreach (var e in inputDS.data)
+			{
+				if(inputDS.GetProperty(e.Key, "District") == _deliveryDistrict)
+				{
+					// Валидация входных данных
+                    DateTime dTime = DateTime.Now;
+                    try
+                    {
+                        dTime = DateTime.Parse(inputDS.GetProperty(e.Key, "DeliveryTime"));
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Некорректный формат времени/время доставки не указано в строке " + e.Key);
+                        LogDS.SetProperty(TimeToFormatedString(DateTime.Now), DateTime.Now.TimeOfDay.ToString(), "	resolved error in input data: Line " + e.Key + " skipped");
+                        LogDS.Save(_deloveryLog);
+                        continue;
+                    }
+                    if (inputDS.GetPropertyAsFloat(e.Key, "Weight") <= 0.0f)
+                    {
+                        Console.WriteLine("Некорректный вес " + e.Key);
+                        LogDS.SetProperty(TimeToFormatedString(DateTime.Now), DateTime.Now.TimeOfDay.ToString(), "	resolved error in input data: Line " + e.Key + " skipped");
+                        LogDS.Save(_deloveryLog);
+                        continue;
+                    }
+                    if (inputDS.GetProperty(e.Key, "District").Length <= 0)
+                    {
+                        Console.WriteLine("Некорректный район " + e.Key);
+                        LogDS.SetProperty(TimeToFormatedString(DateTime.Now), DateTime.Now.TimeOfDay.ToString(), "	resolved error in input data: Line " + e.Key + " skipped");
+                        LogDS.Save(_deloveryLog);
+                        continue;
+                    }
+                    double dif = dTime.Subtract(deliveryTime).TotalMinutes;
+					if(dif>0.0 && dif <= 30.0)
+                    {
+						foreach (var p in e.Value)
+							outputDS.SetProperty(e.Key, p.Key, p.Value);
+                        LogDS.SetProperty(TimeToFormatedString(DateTime.Now), DateTime.Now.TimeOfDay.ToString(), "	output entry added");
+                        LogDS.Save(_deloveryLog);
+                    }
+                }
+			}
+
+			outputDS.Save(_deliveryOrder);
+            Console.WriteLine("Результат фильтрации сохранён в " + _deliveryOrder);
+            LogDS.SetProperty(TimeToFormatedString(DateTime.Now), DateTime.Now.TimeOfDay.ToString(), "	Output saved");
+            LogDS.Save(_deloveryLog);
+
+            LogDS.SetProperty(TimeToFormatedString(DateTime.Now), DateTime.Now.TimeOfDay.ToString(), "	exiting");
+            LogDS.Save(_deloveryLog);
+
+        }
+	}
+}
